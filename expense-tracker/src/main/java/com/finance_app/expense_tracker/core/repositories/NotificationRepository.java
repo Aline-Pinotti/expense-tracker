9cfce91cf678a92eaa472f8929d3c35643d8906f
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2026-04-22 20:28:00 -0300

    feature (UPDATE): add findUnreadNotifications in NotificationRepository

--- expense-tracker/src/main/java/com/finance_app/expense_tracker/core/repositories/NotificationRepository.java
+++ expense-tracker/src/main/java/com/finance_app/expense_tracker/core/repositories/NotificationRepository.java
@@ -2,11 +2,15 @@ package com.finance_app.expense_tracker.core.repositories;
 
 import com.finance_app.expense_tracker.core.entities.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+    @Query("SELECT n FROM Notification n WHERE n.readAt IS NULL")
+    List<Notification> findUnreadNotifications();
 }
 
