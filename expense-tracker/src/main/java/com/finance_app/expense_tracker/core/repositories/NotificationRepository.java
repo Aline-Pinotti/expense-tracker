f7c11460d4d5d710c4f3ebeac37256b86b128a91
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2026-05-09 17:27:20 -0300

    feature: UPDATE cleaning imports from repository

--- expense-tracker/src/main/java/com/finance_app/expense_tracker/core/repositories/NotificationRepository.java
+++ expense-tracker/src/main/java/com/finance_app/expense_tracker/core/repositories/NotificationRepository.java
@@ -4,11 +4,8 @@ import com.finance_app.expense_tracker.core.entities.Notification;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
